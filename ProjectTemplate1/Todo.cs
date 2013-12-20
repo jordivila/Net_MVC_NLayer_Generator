0b81abdb5c37d3889e891415fd3a1f7a0eb740aa
Jordi Vila <jordi.vila@gmail.com>
2013-12-20 13:34:14 +0100

    Todo List Added

---
+++ ProjectTemplate1/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIX_MVC_Layered_ProjectTemplate
+{
+    class TodoList
+    {
+        ///TODO: Create Template.WCF.Unity & Template.UI.Web.Unity
+        ///TODO: Template tests should use testsettings solution file
+    }
+}
