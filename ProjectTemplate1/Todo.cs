db8836bc8f421c1b56a086264994d34cf0db0213
Jordi Vila <jordi.vila@gmail.com>
2014-01-16 17:20:14 +0100

    Todo Task Added

--- ProjectTemplate1/Todo.cs
+++ ProjectTemplate1/Todo.cs
@@ -8,7 +8,27 @@ namespace VSIX_MVC_Layered_ProjectTemplate
 {
     class TodoList
     {
-        ///TODO: Create Template.WCF.Unity & Template.UI.Web.Unity
         ///TODO: Template tests should use testsettings solution file
+        ///
+        /*
+         * Los metodos de UserReuqest qyue guardan valores para un Thread
+         * pierden el contexto cuando se hace un proceso Asincrono
+         * 
+         * Implementar esto en las propiedades que sean "per Thread" o que dependan de la peticion
+         * 
+        public int? AuditoriaLogId
+        {
+            get
+            {
+                return (int?)Thread.GetData(Thread.GetNamedDataSlot(SepaRequestModel_Keys.WcfFormsAuditoriaKey));
+                //return this.Context.Items[SepaRequestModel_Keys.WcfFormsAuditoriaKey] == null ? null : (int?)this.Context.Items[SepaRequestModel_Keys.WcfFormsAuditoriaKey];
+            }
+            set
+            {
+                Thread.SetData(Thread.GetNamedDataSlot(SepaRequestModel_Keys.WcfFormsAuditoriaKey), value);
+                //this.Context.Items[SepaRequestModel_Keys.WcfFormsAuditoriaKey] = value;
+            }
+        }         
+         */
     }
 }
